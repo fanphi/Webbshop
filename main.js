ba42a1d722c6565108509d8c5238b5ac90a6fb68
Daodac-Haze <alkhodaryhazimcsgo@gmail.com>
2022-02-18 12:18:57 +0100

    adding books to localstorage

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+function addToCart(bookId) {
+  var items = JSON.parse(localStorage.getItem("cartItems")) || [];
+
+  var item = items.find((item) => item.book === bookId);
+
+  if (item) {
+    item.count += 1;
+  } else {
+    items.push({
+      book: bookId,
+      count: 1,
+    });
+  }
+  localStorage.setItem("cartItems", JSON.stringify(items));
+  console.log(items);
+}
+
 function readJson() {
   fetch("./data/products.json")
     .then((response) => {
@@ -35,7 +52,7 @@ function readJson() {
               <div class="product-image-container"><img class="book-img" src="${item.src}"</img></div>
             <div class="text-container-product-page"><div class="title-container"><h2 class="book-title">${item.name}, ${item.auth}</h2>
             <h3 class="category-price"><a class="category-link" href="index.html?category=${item.categ}">${item.categ}</a> ${item.price} kr</h3></div><div class="desc-container"><p class="book-desc-text">${item.desc}</div></div>
-            </div><div class="buy-button-container product-info-page"> <button class="buy btn">ADD TO CART</button></div></div>`;
+            </div><div class="buy-button-container product-info-page"> <button book-id="${item.id}" class="buy btn">ADD TO CART</button></div></div>`;
               productWrapper.insertAdjacentHTML("beforeend", newProduct);
             }
           });
@@ -71,13 +88,21 @@ function readJson() {
             <h3 class="book-author">-${item.author}</h3>
             <h3 class="category-price">${item.price} kr</h3>
             <a class="more-info" href="product.html?bookid=${item.id}">More information</a>
-            <button class="buy btn">ADD TO CART</button>
+            <button book-id="${item.id}" class="buy btn">ADD TO CART</button>
           </div>`;
               productWrapper.insertAdjacentHTML("beforeend", newProduct);
             });
           }
         });
       }
+
+      const buyButton = document.querySelectorAll(".buy");
+
+      buyButton.forEach((el) =>
+        el.addEventListener("click", (event) => {
+          addToCart(el.getAttribute("book-id"));
+        })
+      );
     })
     .catch(function () {
       console.log("error");
