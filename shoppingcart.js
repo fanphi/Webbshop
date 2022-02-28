f3b21c2de8d6835dc1c736462716bb9ff7791404
fanphi <fannyp90@hotmail.com>
2022-02-28 09:54:20 +0100

    added comments to shoppingcart.html and shoppingcart.js

--- shoppingcart.js
+++ shoppingcart.js
@@ -1,3 +1,4 @@
+//Om varukorgen är tom så står det så och du kan inte gå vidare till checkout
 if (localStorage.getItem("cartItems") === null) {
   const cartItemWrapper = document.getElementById("productContainer");
   const totalPrice = `<p class="total-p">
@@ -7,12 +8,13 @@ if (localStorage.getItem("cartItems") === null) {
   cartItemWrapper.insertAdjacentHTML("beforeend", totalPrice);
 }
 
+//Knapp som leder till checkout-formulär
 let myCheckoutButton = document.querySelector("#cart-checkout-btn");
 
 myCheckoutButton.addEventListener("click", () => {
   if (localStorage.getItem("cartItems") === null) {
     alert("You need to add an item to the shopping cart!");
   } else {
-    window.location.href = "./checkout.html?page=checkout";
+    window.location.href = "./checkout.html";
   }
 });
