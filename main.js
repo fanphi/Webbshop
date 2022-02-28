569d5819f7fe4290adb7580cf507fc8369056953
AeosKayl <prosium@gmail.com>
2022-02-28 11:08:57 +0100

    made some minor corrections

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//creates localstorage cart items if it does not exist when u add a book to the cart
+//creates localstorage cart items if it does not exist when you add a book to the cart
 function addToCart(bookId) {
   const items = JSON.parse(localStorage.getItem("cartItems")) || [];
   const item = items.find((item) => item.book === bookId);
@@ -48,7 +48,7 @@ function readJson() {
       } else {
         productWrapper.innerHTML = "";
         let pageCheck = params.get("page");
-        //checks the parameters for what page it is and run specific function
+        //checks the parameters for what page it is and runs a specific function
         switch (pageCheck) {
           case "categorypage":
             generateProducts(json);
@@ -187,7 +187,7 @@ function readJson() {
           });
           if (countB < 1) {
             const totalPrice = `<p class="total-p">
-            Din varukorg är tom!
+            Your cart is empty!
             </p>`;
             document.getElementById("cart-checkout-btn").disabled = true; //Fredrika
             cartItemWrapper.insertAdjacentHTML("beforeend", totalPrice);
@@ -236,7 +236,7 @@ readJson();
 if (localStorage.getItem("currentUser") !== null) {
   const checkOutFormElem = document.querySelector("#checkoutForm");
   let currentU = JSON.parse(localStorage.getItem("currentUser"));
-  //checks if form elements excists
+  //checks if form elements exists
   if (checkOutFormElem) {
     let autoFillFields = document.querySelectorAll("[autofill]");
     //gets all the fields that need to get filled in if user is logged in and dynamically adds that information
